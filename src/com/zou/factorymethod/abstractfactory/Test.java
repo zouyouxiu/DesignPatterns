d4f3370fb2e0bbea03b3a90d3de8d2e49ca9a0f0
zouyouxiu <1842036736@qq.com>
2021-01-19 21:55:54 +0800

    注释

--- src/com/zou/factorymethod/abstractfactory/Test.java
+++ src/com/zou/factorymethod/abstractfactory/Test.java
@@ -10,7 +10,8 @@ package com.zou.factorymethod.abstractfactory;
  */
 public class Test {
     public static void main(String[] args) {
-        //抽象工厂优势：产品一族好扩展，产品维度不好扩展 例如：如果想给特定的产品一族新增一个产品属性，那么所有的产品一族都需要修改
+        //抽象工厂优势：产品一族好扩展，产品维度不好扩展 例如：如果想给特定的产品一族新增一个产品，那么所有的产品一族都需要修改
+        //工厂方法优势：产品维度好扩展，可以随意新增产品，不会影响到别的产品
         //AbstractFactory abstractFactory = new DogFactory();
         AbstractFactory abstractFactory = new CatFactory();
         Animal animal = abstractFactory.getAnimal();
